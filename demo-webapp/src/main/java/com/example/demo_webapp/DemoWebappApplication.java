a3167e52c558f840bb97683f649b930ebcbd9839
rajaniekunde <151899905+rajaniekunde@users.noreply.github.com>
2025-01-30 01:14:05 +0530

    spring webapp

--- demo-webapp/src/main/java/com/example/demo_webapp/DemoWebappApplication.java
+++ demo-webapp/src/main/java/com/example/demo_webapp/DemoWebappApplication.java
@@ -3,7 +3,7 @@ package com.example.demo_webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class DemoWebappApplication {
 
 	public static void main(String[] args) {
--- demo-webapp/src/main/java/com/example/demo_webapp/HelloController.java
+++ demo-webapp/src/main/java/com/example/demo_webapp/HelloController.java
@@ -1,12 +1,14 @@
-package com.example.demowebapp;
+package com.example.demo_webapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
-        return "Hello, World! This is a demo Spring Boot web application.";
+        return "Hello, this is a demo Spring Boot application!";
     }
 }
